e37410cbd27e6211aba3f3a7d1b1aab24f7ab580
Nekorobi <177852383+nekorobi@users.noreply.github.com>
2024-11-06 14:30:51 +0900

    test: --validate

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+cd ${0%/*}; set -e
+echo test.sh:
+export XDG_CACHE_HOME=$(pwd)/tmp
+
+echo '  --validate:'
+../sf6stats.sh --validate
+
+rm -r "$XDG_CACHE_HOME/sf6stats"
+
+echo success: test.sh
